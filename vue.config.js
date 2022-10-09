47fcda8d4070c7fa3bda02bc8d2c2da470848b7f
Michal Geci <michal.geci.1995@gmail.com>
2022-10-09 13:48:43 +0200

    Deploy script

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath: '/frasier-random/',
   pwa: {
     name: 'Frasier episode',
     icons: {
