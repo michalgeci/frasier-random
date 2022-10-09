6d765cf5ea8111205885bab7c8e460099758ee45
Michal Geci <michal.geci.1995@gmail.com>
2022-10-09 14:00:32 +0200

    change icons

--- vue.config.js
+++ vue.config.js
@@ -4,6 +4,7 @@ module.exports = defineConfig({
   publicPath: '/frasier-random/',
   pwa: {
     name: 'Frasier episode',
+    themeColor: "#ffffff",
     icons: {
       favicon32: 'img/icons/favicon-32x32.png',
       favicon16: 'img/icons/favicon-16x16.png',
